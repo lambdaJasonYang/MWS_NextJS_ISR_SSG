de399903483a93cde7531d56c6fdb3f7137d6762
JY <jy1486@nyu.edu>
2022-10-13 00:10:42 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,19 +5,7 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 const isProd = process.env.myEnv === "prod";
 console.log(process.env)
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      /* development only config options here */
-      reactStrictMode: true,
-      // assetPrefix: isProd ? undefined : "/",
-      // basePath: isProd ? undefined : "/",
-      experimental: {
-        images: {
-          unoptimized: true,
-        },
-      }
-    }
-  }
+  if (phase === PHASE_DEVELOPMENT_SERVER){ isProd = false };  
   return {
     /* config options for all phases except development here */
     reactStrictMode: true,
